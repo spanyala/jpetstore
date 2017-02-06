08ece0d4520b5c8dfdd69029fde46d1bee32f38f
lweitzel <lweitzel@crgt.com>
2017-02-06 13:16:44 -0500

    axe508

---
+++ axe/spec/testMainPage2.js
@@ -0,0 +1,50 @@
+var selenium = require('selenium-webdriver'),
+    AxeBuilder = require('axe-webdriverjs'),
+    Key = selenium.Key;
+
+var util = require('util');
+
+var driver;
+
+describe('Petstore demo', function() {
+
+    beforeEach(function(done) {
+        driver = new selenium.Builder()
+            .forBrowser('chrome')
+            .build();
+
+        driver.get('http://jp-dev.salientcrgt-devops.com/jpetstore/actions/Catalog.action')
+            .then(function () {
+                done();
+            });
+    });
+
+    // Close website after each test is run (so it is opened fresh each time)
+    afterEach(function(done) {
+        driver.quit().then(function () {
+            done();
+        });
+    });
+
+
+	    it('should just fetch the home page and analyze the whole page', function (done) {
+        driver.findElement(selenium.By.css('body.home')).then(function (element) {
+            return element.getAttribute('class').then(function (className) {
+                expect(className.indexOf('home')).toBe(0);
+            });
+        }).then(function () {
+                AxeBuilder(driver)
+                    .analyze(function(results) {
+                        console.log('Accessibility Violations on the Catalog page: ', results.violations.length);
+                        if (results.violations.length > 0) {
+							for (var i = 0; i < results.violations.length; i++) {
+								console.log(results.violations[i].description);
+							}
+                        }
+                        expect(results.violations.length).toBe(0);
+                        done();
+                    })
+            });
+    });
+	
+});
