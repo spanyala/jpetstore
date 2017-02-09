b29db596495bea3e0942f28528177135b90de749
MarkNarvaez <Mark.Narvaez@salientcrgt.com>
2017-02-08 22:57:50 -0500

    Update testAllPages.js

--- axe/spec/testAllPages.js
+++ axe/spec/testAllPages.js
@@ -336,3 +336,111 @@ describe('Radio button demo', function() {
             });
     });
 });
+
+describe('Radio button demo', function() {
+
+    beforeEach(function(done) {
+        driver = new selenium.Builder()
+            .forBrowser('chrome')
+            .build();
+
+        driver.get('http://jp-dev.salientcrgt-devops.com/jpetstore/actions/Catalog.action')
+            .then(function () {
+                done();
+            });
+    });
+
+    // Close website after each test is run (so it is opened fresh each time)
+    afterEach(function(done) {
+        driver.quit().then(function () {
+            done();
+        });
+    });
+
+
+    it('should fetch the estoremap in the main page and analyze it', function (done) {
+		driver.findElement(selenium.By.name("estoremap"))
+            .then(function () {
+                AxeBuilder(driver)
+                    .analyze(function(results) {
+                        console.log('Accessibility Violations: ', results.violations.length);
+                        if (results.violations.length > 0) {
+                            //console.log(util.inspect(results.violations, true, null));
+			    var stream = fs.createWriteStream("testPetMain.html");
+       			    stream.once('open', function(fd) {
+        			for (i=0; i<results.violations.length; i++) {
+         				stream.write("Help: " + results.violations[i].help + "<br/>\n");
+         				stream.write("Description:  " + results.violations[i].description + "<br/>\n");
+         				//stream.write("impact:  " + results.violations[i].impact + "<br/>\n");
+         				var myHtml = results.violations[i].nodes[0].html;
+         				myHtml = myHtml.split("<").join("&lt;");
+         				myHtml = myHtml.split(">").join("&gt;");
+         				stream.write("&nbsp;&nbsp;&nbsp;&nbsp;HTML:  " + myHtml + "<br/>\n");
+         				for (j=0; j< results.violations[i].nodes[0].any.length; j++) {
+          					stream.write("&nbsp;&nbsp;&nbsp;&nbsp;Message:  " + results.violations[i].nodes[0].any[j].message + "<br/>\n");
+         				}
+	 				stream.write("<br/>\n");
+        			}
+        			stream.end();
+       			    });
+                        }
+                        expect(results.violations.length).toBe(0);
+                        done();
+                    })
+            });
+    });
+});
+
+describe('Radio button demo', function() {
+
+    beforeEach(function(done) {
+        driver = new selenium.Builder()
+            .forBrowser('chrome')
+            .build();
+
+        driver.get('http://jp-dev.salientcrgt-devops.com/jpetstore/actions/Catalog.action')
+            .then(function () {
+                done();
+            });
+    });
+
+    // Close website after each test is run (so it is opened fresh each time)
+    afterEach(function(done) {
+        driver.quit().then(function () {
+            done();
+        });
+    });
+
+
+    it('should fetch the Content button in the main page and analyze it', function (done) {
+		driver.findElement(selenium.By.id("Content"))
+            .then(function () {
+                AxeBuilder(driver)
+                    .analyze(function(results) {
+                        console.log('Accessibility Violations: ', results.violations.length);
+                        if (results.violations.length > 0) {
+                            //console.log(util.inspect(results.violations, true, null));
+			    var stream = fs.createWriteStream("testPetMainContent.html");
+       			    stream.once('open', function(fd) {
+        			for (i=0; i<results.violations.length; i++) {
+         				stream.write("Help: " + results.violations[i].help + "<br/>\n");
+         				stream.write("Description:  " + results.violations[i].description + "<br/>\n");
+         				//stream.write("impact:  " + results.violations[i].impact + "<br/>\n");
+         				var myHtml = results.violations[i].nodes[0].html;
+         				myHtml = myHtml.split("<").join("&lt;");
+         				myHtml = myHtml.split(">").join("&gt;");
+         				stream.write("&nbsp;&nbsp;&nbsp;&nbsp;HTML:  " + myHtml + "<br/>\n");
+         				for (j=0; j< results.violations[i].nodes[0].any.length; j++) {
+          					stream.write("&nbsp;&nbsp;&nbsp;&nbsp;Message:  " + results.violations[i].nodes[0].any[j].message + "<br/>\n");
+         				}
+	 				stream.write("<br/>\n");
+        			}
+        			stream.end();
+			    });
+                        }
+                        expect(results.violations.length).toBe(0);
+                        done();
+                    })
+            });
+    });
+});
