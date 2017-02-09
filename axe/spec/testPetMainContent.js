f59e11ecf20cde90d0e833704d67395cc3f990de
MarkNarvaez <Mark.Narvaez@salientcrgt.com>
2017-02-09 13:01:25 -0500

    Update testPetMainContent.js

--- axe/spec/testPetMainContent.js
+++ axe/spec/testPetMainContent.js
@@ -44,9 +44,9 @@ describe('Radio button demo', function() {
          				stream.write("Description:  " + results.violations[i].description + "<br/>\n");
          				//stream.write("impact:  " + results.violations[i].impact + "<br/>\n");
 					stream.write("<br/>\n");
-					var violation_num = i+1;
-					stream.write("&nbsp;&nbsp;&nbsp;&nbsp;Violation #" + violation_num + "<br/>\n");
          				for (j=0; j< results.violations[i].nodes.length; j++) {
+						var violation_num = j+1;
+						stream.write("&nbsp;&nbsp;&nbsp;&nbsp;Violation #" + violation_num + "<br/>\n");
 						var myHtml = results.violations[i].nodes[j].html;
          					myHtml = myHtml.split("<").join("&lt;");
          					myHtml = myHtml.split(">").join("&gt;");
