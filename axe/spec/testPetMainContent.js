4e8c7cd1c02a40eae048a7d31ec1a315303ff75a
MarkNarvaez <Mark.Narvaez@salientcrgt.com>
2017-02-08 22:46:53 -0500

    Update testPetMainContent.js
    Added logic to write results to HTML file

--- axe/spec/testPetMainContent.js
+++ axe/spec/testPetMainContent.js
@@ -6,6 +6,8 @@ var util = require('util');
 
 var driver;
 
+var fs = require('fs');
+
 describe('Radio button demo', function() {
 
     beforeEach(function(done) {
@@ -34,7 +36,24 @@ describe('Radio button demo', function() {
                     .analyze(function(results) {
                         console.log('Accessibility Violations: ', results.violations.length);
                         if (results.violations.length > 0) {
-                            console.log(util.inspect(results.violations, true, null));
+                            //console.log(util.inspect(results.violations, true, null));
+			    var stream = fs.createWriteStream("testPetMainContent.html");
+       			    stream.once('open', function(fd) {
+        			for (i=0; i<results.violations.length; i++) {
+         				stream.write("Help: " + results.violations[i].help + "<br/>\n");
+         				stream.write("Description:  " + results.violations[i].description + "<br/>\n");
+         				//stream.write("impact:  " + results.violations[i].impact + "<br/>\n");
+         				var myHtml = results.violations[i].nodes[0].html;
+         				myHtml = myHtml.split("<").join("&lt;");
+         				myHtml = myHtml.split(">").join("&gt;");
+         				stream.write("&nbsp;&nbsp;&nbsp;&nbsp;HTML:  " + myHtml + "<br/>\n");
+         				for (j=0; j< results.violations[i].nodes[0].any.length; j++) {
+          					stream.write("&nbsp;&nbsp;&nbsp;&nbsp;Message:  " + results.violations[i].nodes[0].any[j].message + "<br/>\n");
+         				}
+	 				stream.write("<br/>\n");
+        			}
+        			stream.end();
+			    });
                         }
                         expect(results.violations.length).toBe(0);
                         done();
