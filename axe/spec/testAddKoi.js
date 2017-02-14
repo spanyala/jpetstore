b76ae0a7d74854f080e81b20a584f9d654b4d13c
MarkNarvaez <Mark.Narvaez@salientcrgt.com>
2017-02-14 08:15:28 -0500

    Update testAddKoi.js

--- axe/spec/testAddKoi.js
+++ axe/spec/testAddKoi.js
@@ -41,7 +41,7 @@ describe('Radio button demo', function() {
        			    stream.once('open', function(fd) {
         			for (i=0; i<results.violations.length; i++) {
          				var violation_num = i+1;
-					stream.write("<div style='color:#0000FF'>Violation #" + violation_num + "/" + results.violations.length + "</div>\n");
+					stream.write('<div style="color:#0000FF">Violation #' + violation_num + '/' + results.violations.length + "</div>\n");
          				stream.write("Help: " + results.violations[i].help + "<br/>\n");
 					//stream.write("<a href=''" + results.violations[i].helpUrl + "''>More Info</a><br/>\n");
          				stream.write("Description:  " + results.violations[i].description + "<br/>\n");
